914ec07f806c8e6cae2956ed84b97aba06d86a91
matheuswww <teteus.30.08.07@outlook.com>
2025-01-27 14:39:52 -0300

    commenting raw mode flags

--- main.c
+++ main.c
@@ -24,12 +24,51 @@ void enableRawMode() {
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
+
+  // Input flags configuration
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  /*
+  * BRKINT: Prevents a SIGINT signal from being sent when a break condition is detected.
+  * ICRNL: Disables the conversion of '\r' (carriage return) into '\n' (newline).
+  * INPCK: Disables input parity checking.
+  * ISTRIP: Prevents stripping the 8th bit of input, allowing full byte data.
+  * IXON: Disables software flow control (XON/XOFF).
+  */
+
+  // Output flags configuration
   raw.c_oflag &= ~(OPOST);
+  /*
+  * OPOST: Disables output processing, such as converting '\n' into '\r\n'.
+  */
+
+  // Control flags configuration
   raw.c_cflag &= ~(CS8);
+  /*
+  * CS8: Disables the 8-bit per character setting.
+  * However, this might be an issue, as CS8 is usually enabled to work with 8-bit data.
+  */
+
+  // Local flags configuration
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  /*
+  * ECHO: Disables echoing of typed characters to the terminal.
+  * ICANON: Disables canonical mode, allowing input to be read byte by byte.
+  * IEXTEN: Disables extended input features, such as Ctrl-V (literal next).
+  * ISIG: Disables the generation of signals like SIGINT (Ctrl-C) and SIGTSTP (Ctrl-Z).
+  */
+
+  // Control characters configuration
   raw.c_cc[VMIN] = 0;
+  /*
+  * VMIN: Sets the minimum number of bytes needed for a read operation to return.
+  * Here, 0 means the read will return immediately, even if no data is available.
+  */
+
   raw.c_cc[VTIME] = 10 * 4;
+  /*
+  * VTIME: Sets the maximum wait time (in tenths of a second) for input data.
+  * Here, it is set to 40 tenths of a second (4 seconds).
+  */
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
